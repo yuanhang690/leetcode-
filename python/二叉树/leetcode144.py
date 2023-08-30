8576bb882e5c7ec630e9c2a072a3d148d4f4cbd0
yuanhang690 <18907612085@163.com>
2023-08-30 19:08:50 +0800

    leetcode144

---
+++ python/二叉树/leetcode144.py
@@ -0,0 +1,14 @@
+"""
+二叉树的前序遍历
+
+"""
+
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        left = self.preorderTraversal(root.left)
+        right = self.preorderTraversal(root.right)
+
+        return [root.val] + left + right
